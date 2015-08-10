2389f520869f06e08bc6039eb54e33118bcf2b7b
Jacob Yong <yongsanghoon@gmail.com>
2015-08-10 22:28:31 +0000

    Changed root to go to /static_pages/home instead of users#index (commented out).

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
 
   resources :microposts
   resources :users
-  root 'users#index'
+  #root 'users#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
